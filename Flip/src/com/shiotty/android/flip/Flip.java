bf4e5a63a2c253b1cfe53cee7bab406effa53a2b
shiotty <shiotty@gmail.com>
2010-06-18 01:02:47 +0900

    Add Android project File

---
+++ Flip/src/com/shiotty/android/flip/Flip.java
@@ -0,0 +1,154 @@
+package com.shiotty.android.flip;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+public class Flip extends Activity implements
+        AdapterView.OnItemClickListener {
+    private ListView mPhotosList;
+    private ViewGroup mContainer;
+    private RelativeLayout mRelativeLayout;
+	
+    // Names of the photos we show in the list
+    private static final String[] PHOTOS_NAMES = new String[] {
+            "Lyon",
+            "Livermore",
+            "Tahoe Pier",
+            "Lake Tahoe",
+            "Grand Canyon",
+            "Bodie"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.animations_main_screen);
+
+        mPhotosList = (ListView) findViewById(android.R.id.list);
+        mContainer = (ViewGroup) findViewById(R.id.container);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.content_layout);
+        
+        // Prepare the ListView
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, PHOTOS_NAMES);
+
+        mPhotosList.setAdapter(adapter);
+        mPhotosList.setOnItemClickListener(this);
+
+        // 戻るボタンの設定
+        Button mBackBtn = (Button) findViewById(R.id.back_btn);
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// 0度から-90度に移動する
+				applyRotation(-1, 0, -90);
+            }
+        });
+        
+        // Since we are caching large views, we want to keep their cache
+        // between each animation
+        mContainer.setPersistentDrawingCache(ViewGroup.PERSISTENT_ANIMATION_CACHE);
+    }
+
+    /**
+     * Setup a new 3D rotation on the container view.
+     *
+     * @param position the item that was clicked to show a picture, or -1 to show the list
+     * @param start the start angle at which the rotation must begin
+     * @param end the end angle of the rotation
+     */
+    private void applyRotation(int position, float start, float end) {
+        // Find the center of the container
+        final float centerX = mContainer.getWidth() / 2.0f;
+        final float centerY = mContainer.getHeight() / 2.0f;
+
+        // Create a new 3D rotation with the supplied parameter
+        // The animation listener is used to trigger the next animation
+        final Rotate3dAnimation rotation =
+                new Rotate3dAnimation(start, end, centerX, centerY, 310.0f, true);
+        rotation.setDuration(500);
+        rotation.setFillAfter(true);
+        rotation.setInterpolator(new AccelerateInterpolator());
+        rotation.setAnimationListener(new DisplayNextView(position));
+
+        mContainer.startAnimation(rotation);
+    }
+
+    public void onItemClick(AdapterView parent, View v, int position, long id) {
+        applyRotation(position, 0, 90);
+    }
+
+    /**
+     * This class listens for the end of the first half of the animation.
+     * It then posts a new action that effectively swaps the views when the container
+     * is rotated 90 degrees and thus invisible.
+     */
+    private final class DisplayNextView implements Animation.AnimationListener {
+        private final int mPosition;
+
+        private DisplayNextView(int position) {
+            mPosition = position;
+        }
+
+        public void onAnimationStart(Animation animation) {
+        }
+
+        public void onAnimationEnd(Animation animation) {
+            mContainer.post(new SwapViews(mPosition));
+        }
+
+        public void onAnimationRepeat(Animation animation) {
+        }
+    }
+
+    /**
+     * This class is responsible for swapping the views and start the second
+     * half of the animation.
+     */
+    private final class SwapViews implements Runnable {
+        private final int mPosition;
+
+        public SwapViews(int position) {
+            mPosition = position;
+        }
+
+        public void run() {
+            final float centerX = mContainer.getWidth() / 2.0f;
+            final float centerY = mContainer.getHeight() / 2.0f;
+            Rotate3dAnimation rotation;
+            
+            if (mPosition > -1) {
+                mPhotosList.setVisibility(View.GONE);
+                mRelativeLayout.setVisibility(View.VISIBLE);
+                mRelativeLayout.requestFocus();
+                // -90度から0度に回転する。
+                rotation = new Rotate3dAnimation(-90, 0, centerX, centerY, 310.0f, false);
+                //rotation = new Rotate3dAnimation(90, 180, centerX, centerY, 310.0f, false);
+                
+            } else {
+            	mRelativeLayout.setVisibility(View.GONE);
+                mPhotosList.setVisibility(View.VISIBLE);
+                mPhotosList.requestFocus();
+
+                rotation = new Rotate3dAnimation(90, 0, centerX, centerY, 310.0f, false);
+            }
+
+            rotation.setDuration(500);
+            rotation.setFillAfter(true);
+            rotation.setInterpolator(new DecelerateInterpolator());
+
+            mContainer.startAnimation(rotation);
+        }
+    }
+
+}
